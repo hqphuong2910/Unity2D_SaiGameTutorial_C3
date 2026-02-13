704e049e2578bc861e1caefeaf7f3f19e4bc88a8
Hoang Quoc Phuong <qphwng2005@gmail.com>
2026-02-13 21:42:48 +0700

    E5 - Code dependency, sự phụ thuộc trong code
    - Tách biệt logic di chuyển, xoay tàu, v.v thành các hàm riêng trong `ShipMovement.cs`
    để dễ quản lý và tái sử dụng.

--- Assets/_Game/Scripts/ShipMovement.cs
+++ Assets/_Game/Scripts/ShipMovement.cs
@@ -4,16 +4,38 @@ namespace _Game.Scripts
 {
     public class ShipMovement : MonoBehaviour
     {
-        [SerializeField] private Vector3 worldPosition;
+        [SerializeField] private Vector3 targetPosition;
         [SerializeField] private float moveSpeed = 1f;
+
+        private void Update()
+        {
+            UpdateTargetPosition();
+        }
 
         private void FixedUpdate()
         {
-            worldPosition = InputManager.Instance.MouseWorldPosition;
-            worldPosition.z = 0;
+            HandleMovement();
+            HandleRotation();
+        }
 
-            var newPos = Vector3.Lerp(transform.parent.position, worldPosition, moveSpeed * Time.fixedDeltaTime);
+        private void UpdateTargetPosition()
+        {
+            targetPosition = InputManager.Instance.MouseWorldPosition;
+            targetPosition.z = 0;
+        }
+
+        private void HandleMovement()
+        {
+            var newPos = Vector3.Lerp(transform.parent.position, targetPosition, moveSpeed * Time.fixedDeltaTime);
             transform.parent.position = newPos;
+        }
+
+        private void HandleRotation()
+        {
+            var diff = targetPosition - transform.parent.position;
+            diff.Normalize();
+            var rotZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            transform.parent.rotation = Quaternion.Euler(0f, 0f, rotZ + 90);
         }
     }
 }
