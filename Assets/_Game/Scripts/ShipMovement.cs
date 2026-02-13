a00ef61bafacfa5146a749312a66b8a9f78bcb6b
Hoang Quoc Phuong <qphwng2005@gmail.com>
2026-02-13 20:54:36 +0700

    E2 - Tàu di chuyển theo con trỏ chuột

---
+++ Assets/_Game/Scripts/ShipMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace _Game.Scripts
+{
+    public class ShipMovement : MonoBehaviour
+    {
+        [SerializeField] private Vector3 worldPosition;
+        [SerializeField] private float moveSpeed = 1f;
+
+        private void FixedUpdate()
+        {
+            worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0;
+
+            var newPos = Vector3.Lerp(transform.position, worldPosition, moveSpeed * Time.fixedDeltaTime);
+            transform.position = newPos;
+        }
+    }
+}
