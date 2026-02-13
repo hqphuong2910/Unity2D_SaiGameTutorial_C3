fb42e36f8768521961285d451f5bf46dfc67665f
Hoang Quoc Phuong <qphwng2005@gmail.com>
2026-02-13 21:58:28 +0700

    E6 - If và Boolean
    - Sử dụng biến boolean `isShooting` làm cờ hiệu kiểm soát trạng thái bắn.
    - Dùng cấu trúc `if` để kiểm tra điều kiện hợp lệ trước khi thực hiện
    hành động bắn.

---
+++ Assets/_Game/Scripts/ShipShooting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Game.Scripts
+{
+    public class ShipShooting : MonoBehaviour
+    {
+        [SerializeField] private bool isShooting = false;
+        [SerializeField] private Transform bulletPrefab;
+
+        private void FixedUpdate()
+        {
+            Shooting();
+        }
+
+        private void Shooting()
+        {
+            if (!isShooting) return;
+
+            Instantiate(bulletPrefab);
+
+            Debug.Log(nameof(Shooting));
+        }
+    }
+}
